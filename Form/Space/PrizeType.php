937e588473cc04c5820f712f4fb8bcbe9f002f6c
lexone <lexone@lexone.lan>
2013-08-08 20:13:36 +0300

    _

--- Form/Space/PrizeType.php
+++ Form/Space/PrizeType.php
@@ -12,7 +12,11 @@ class PrizeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-                ->add('name', 'text')
+                ->add('name', 'text', array(
+                    'attr' => array(
+                        'id' => 'inp'
+                    )
+                ))
                 ->add('flipper', 'choice', array(
                     'choices' => array(
                         '1' => '1',
@@ -20,12 +24,31 @@ class PrizeType extends AbstractType
                         '3' => '3',
                         '4' => '4',
                         '5' => '5',
+                    ),
+                    'attr' => array(
+                        'id' => 'flipInp'
                     )
                 ))
-                ->add('sms', 'text')
-                ->add('message1', 'textarea', array("required" => false))
-                ->add('message2', 'textarea', array("required" => false))
-                ->add('penalty', 'integer')
+                ->add('sms', 'text', array(
+                    'attr' =>array(
+                        'id' => 'inp'
+                    )
+                ))
+                ->add('message1', 'textarea', array("required" => false,
+                    'attr' => array(
+                        'id' => 'txtArea'
+                    )
+                ))
+                ->add('message2', 'textarea', array("required" => false,
+                    'attr' => array(
+                        'id' => 'txtArea'
+                    )
+                ))
+                ->add('penalty', 'integer', array(
+                    'attr' => array(
+                        'id' => 'flipInp'
+                    )
+                ))
                 ->add('imgfile1', 'file', array("required" => false))
                 ->add('imgfile2', 'file', array("required" => false))
         ;
