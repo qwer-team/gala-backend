80e6838c9d2b0c22e3170b6b0ed01cc5564f85a1
itcompany <itcompany@itc.(none)>
2013-08-08 10:26:35 +0300

    _

--- Form/Filter/SearchType.php
+++ Form/Filter/SearchType.php
@@ -20,33 +20,66 @@ class SearchType extends AbstractType
         $builder
                 ->add('messageId', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId',
+                        'placeholder' => 'id'
+                    )
                 ))
                 ->add('userId', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'userId',
+                        'placeholder' => 'User'
+                    )
                 ))
                 ->add('theme', 'choice', array(
                     'required' => false,
                     'choices' => $this->themes,
+                    'empty_value' => 'Тема',
+                    'attr' => array(
+                        'id' => 'theme'
+                    )
                 ))
                 ->add('title', 'text', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'title',
+                        'placeholder' => 'Название'
+                    )
                 ))
                 ->add('visible', 'choice', array(
                     'required' => false,
                     'choices' => array(
                         '0' => 'Невидим',
                         '1' => 'Видим',
+                    ),
+                    'empty_value' => 'Видимость',
+                    'empty_data'  => null,
+                    'attr' => array(
+                        'id' => 'visible',
+                        'class' => 'default',
+                        'title' => 'visible',
+                        'value' => ''
                     )
                 ))
                 ->add('moderatorAccepted', 'choice', array(
                     'required' => false,
+                    'empty_value' => 'Не/Принято',
                     'choices' => array(
                         '0' => 'Не принято',
                         '1' => 'Принято',
+                    ),
+                    'attr' => array(
+                        'id' => 'moderator'
                     )
                 ))
                ->add('age', 'choice', array(
                    'required' => false,
+                   'empty_value' => 'Возраст',
+                   'empty_data' => null,
+                   'attr' => array(
+                       'id' => 'age'
+                   ),
                     'choices' => array(
                         '12' => 'до 12',
                         '16' => 'до 16',
@@ -54,8 +87,12 @@ class SearchType extends AbstractType
                         '22' => 'до 22',
                         '23' => 'старше 22',
                     )))
-                ->add('text', 'textarea', array(
+                ->add('text', 'text', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'text',
+                        'placeholder' => 'Текст'
+                    )
                 ))
         ;
     }
--- Form/Info/MessageType.php
+++ Form/Info/MessageType.php
@@ -22,6 +22,15 @@ class MessageType extends AbstractType
         $builder
                 ->add('incPoints', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
+                ))
+                ->add('messageId', 'integer', array(
+                    'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId' 
+                    )
                 ))
                 ->add('question', 'textarea')
                 ->add('answers', 'collection', array(
@@ -36,7 +45,8 @@ class MessageType extends AbstractType
                         '5' => '5',
                     )
                 ))
-                ->add('imgfile', 'file', array("required" => false))
+                ->add('imgfile', 'file', array("required" => false,
+                        ))
                 ->add('imageDelete', 'checkbox', array(
                     'required' => false,
                 ))
@@ -48,54 +58,91 @@ class MessageType extends AbstractType
                 ))
                 ->add('incPointsMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('incOwnElem', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
                 ))
                 ->add('incOwnElemActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('incOwnElemMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
+                    
                 ))
                 ->add('incDurationMinElem', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
                 ))
                 ->add('incDurationMinElemActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('incDurationMinElemMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('incFlipAmount', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
                 ))
                 ->add('incFlipAmountActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('incFlipAmountMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('superjumpAmount', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
                 ))
                 ->add('superjumpAmountActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('superjumpAmountMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('incDurationAllElem', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
                 ))
                 ->add('incDurationAllElemActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('incDurationAllElemMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('decPoints', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
                 ))
                 ->add('decPointsActv', 'checkbox', array(
                     'required' => false,
@@ -105,61 +152,99 @@ class MessageType extends AbstractType
                 ))
                 ->add('decPointsMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('decFlipAmount', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
                 ))
                 ->add('decFlipAmountActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('decFlipAmountMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('superjumpCancelActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('superjumpCancelMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('activeCancelActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('activeCancelMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('firstFlipperActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('firstFlipperMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('blackPointActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('blackPointMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('delElemGroupActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('delElemGroupMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('decDurationAllElem', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
                 ))
                 ->add('decDurationAllElemActv', 'checkbox', array(
                     'required' => false,
                 ))
                 ->add('decDurationAllElemMess', 'textarea', array(
                     'required' => false,
+                    'attr' => array(
+                        'class' => 'width'
+                    )
                 ))
                 ->add('userId', 'integer', array(
                     'required' => false,
                 ))
-                ->add('title', 'text')
+                ->add('title', 'text', array(
+                    'required' => false,
+                    'attr' => array(
+                        'id' => 'title'
+                    )
+                ) )
                 ->add('visible', 'checkbox', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'visible'
+                    )
                 ))
                 ->add('moderatorAccepted', 'checkbox', array(
                     'required' => false,
@@ -167,6 +252,9 @@ class MessageType extends AbstractType
                 ->add('theme', 'choice', array(
                     'required' => false,
                     'choices' => $this->themes,
+                    'attr' => array(
+                        'id' => 'theme'
+                    )
                 ))
                 ->add('age', 'choice', array(
                     'required' => false,
@@ -176,6 +264,9 @@ class MessageType extends AbstractType
                         '18' => 'до 18',
                         '22' => 'до 22',
                         '23' => 'старше 22',
+                    ),
+                    'attr' => array(
+                        'id' => 'age'
                     )
                 ))
                 ->add('text', 'textarea', array(
@@ -184,6 +275,9 @@ class MessageType extends AbstractType
                 ))
                 ->add('jumpsToQuestion', 'integer', array(
                     'required' => false,
+                    'attr' => array(
+                        'id' => 'messageId'
+                    )
                 ))
                 ->add('seconds', 'integer', array(
                     'required' => false,
