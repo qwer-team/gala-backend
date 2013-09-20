7c5b9aa1cff8a330b89882b6dcc83304fbe85552
minout <minouts@gmail.com>
2013-09-20 16:01:31 +0300

    style PrizeType and PointsType form

--- Form/Space/SubtypeType.php
+++ Form/Space/SubtypeType.php
@@ -6,29 +6,34 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class SubtypeType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class SubtypeType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('pointsCount', 'integer')
-            ->add('block', 'checkbox', array('required' => false))
-            ->add('restore', 'checkbox', array('required' => false))
-            ->add('percent')
-            ->add('parameter', 'integer', array('required' => false))
-            ->add('typeId', 'hidden')
-            ->add('id', 'hidden')
-         ;
+                ->add('pointsCount', 'integer')
+                ->add('block', 'checkbox', array(
+                    'required' => false,
+                    'attr' => array('style' => 'margin-left:45%'),
+                ))
+                ->add('restore', 'checkbox', array(
+                    'required' => false,
+                    'attr' => array('style' => 'margin-left:45%'),
+                ))
+                ->add('percent')
+                ->add('parameter', 'integer', array('required' => false))
+                ->add('typeId', 'hidden')
+                ->add('id', 'hidden')
+        ;
     }
 
-    public function getName()
-    {
+    public function getName() {
         return '';
     }
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
-            'csrf_protection'   => false,
+            'csrf_protection' => false,
         ));
     }
+
 }
